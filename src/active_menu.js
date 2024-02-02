563c7488c7e673793d6db26ab0224e27070e6e0f
hwajin <57221951+leehj00@users.noreply.github.com>
2024-02-03 03:28:16 +0900

    edit-themecolor

--- src/active_menu.js
+++ src/active_menu.js
@@ -6,7 +6,7 @@ let activeNavItem = navItems[0];
 
 const options = {
     rootMargin: '-20px 0px 0px 0px',
-    threshold: [0, 0.98],
+    threshold: [0, 0.8, 0.98],
 };
 const observer = new IntersectionObserver(observerCallback, options);
 sections.forEach((section) => observer.observe(section));
@@ -16,11 +16,17 @@ function observerCallback(entries) {
     entries.forEach((entry) => {
         const index = sectionIds.indexOf(`#${entry.target.id}`);
         visibleSections[index] = entry.isIntersecting;
+
         selectLastOne = 
         index === sectionIds.length -1 && 
         entry.isIntersecting && 
         entry.intersectionRatio >= 0.95;
+
+        if(entry.target.id == "skills" && entry.isIntersecting && entry.intersectionRatio >= 0.8) {
+            
+        }
     });
+    
 
     const navIndex = selectLastOne ? sectionIds.length - 1 
     : findFirstIntersecting(visibleSections);
